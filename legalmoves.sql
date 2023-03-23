bd0fb7c8054e4856b04bdc640200b0aea8dcffb1
ContentiShogi <124816610+ContentiShogi@users.noreply.github.com>
2023-03-23 16:21:43 +0530

    Piece skewered to King can't move

--- legalmoves.sql
+++ legalmoves.sql
@@ -4,7 +4,7 @@ DECLARE @sfen NVARCHAR(256) =
 	--ppppkpppp/9/4P4/9/9/9/4p4/9/PPPPKPPPP b Pp 1 --test nifu
 	--4k4/P8/P+R5l1/9/4b4/9/9/+l+b5L1/LB2K2L1 b - 1 --test ranged pieces
 	--1k1l1l3/9/2+B6/9/9/9/9/8+r/L3K4 b rb4g4s4nl18p 1 --don't walk into check
-	'b3r3b/4g4/2+N3l2/3S1S3/rg+l1KG3/3S+R4/6s2/1L7/4L4 b - 1'--test discovered check
+	'b3r3b/4g4/2+N3l2/3S1S3/rg+l1KG3/3S+R4/6s2/1L7/4r4 b - 1'--test discovered check
 	;
 DECLARE @kyokumen dbo.kyokumen_plus
 	;
@@ -19,9 +19,9 @@ INSERT INTO @kyokumenprint(mover,koma,position,next_mover,move_number)
 	;
 --select dbo.f_printKyokumen(@kyokumenprint,0)
 DECLARE @moves TABLE ([mover] [bit] NULL,[koma] [int] NOT NULL,[position] [int] NULL,[moves] [int] NULL,[promotion] [smallint] NULL,[moves_id] [int] NOT NULL
-	,[movesuji] [int] NULL,[movedan] [int] NULL,[movekakupara] [int] NULL,[movekakuorth] [int] NULL,bitmask int null,direction tinyint,distance tinyint null)
+	,[movesuji] [int] NULL,[movedan] [int] NULL,[movekakupara] [int] NULL,[movekakuorth] [int] NULL,bitmask int null,direction int,distance int null)
 --DECLARE @deleted_moves TABLE ([mover] [bit] NULL,[koma] [int] NOT NULL,[position] [int] NULL,[moves] [int] NULL,[promotion] [smallint] NULL,[moves_id] [int] NOT NULL
---	,[movesuji] [int] NULL,[movedan] [int] NULL,[movekakupara] [int] NULL,[movekakuorth] [int] NULL,bitmask int null,direction tinyint,distance tinyint null
+--	,[movesuji] [int] NULL,[movedan] [int] NULL,[movekakupara] [int] NULL,[movekakuorth] [int] NULL,bitmask int null,direction int,distance int null
 --	, [reason] [varchar](500))
 -- Get all possible moves
 INSERT INTO @moves
@@ -47,24 +47,48 @@ select mover,koma,moves,@nextmover,1 from @moves where mover=0
 insert into @kyokumenprint1
 select mover,koma,moves,@nextmover,1 from @moves where mover=1;
 
-with threatranges as
-	( select kyokumen.*,moves._id moves_id,moves.moves,moves.direction,moves.distance,moves.movesuji,moves.movedan,moves.movekakuorth,moves.movekakupara,moves.bitmask
-		from @longrange kyokumen
-		join moves on moves.mover = kyokumen.mover and moves.position = kyokumen.position and moves.koma = kyokumen.koma
-		where kyokumen.position between 11 and 99
-	)
-	, rangelimits as
-	( select threatranges.mover,threatranges.koma,threatranges.position,direction,min(distance)mindist
-		from threatranges
-		join @kyokumen kyokumen on kyokumen.position=threatranges.moves
-		group by threatranges.mover,threatranges.koma,threatranges.position,threatranges.direction
-	)
-	delete moves1
-	from @moves moves1
+DECLARE @threatranges AS TABLE (mover bit,koma smallint, position smallint,next_mover int null,move_number int null,suji int,dan int, kakuparallel int, kakuorthogonal int
+	,	[moves_id] [int] NOT NULL,[moves] [int] NULL,direction int,distance int null,[movesuji] [int] NULL,[movedan] [int] NULL,[movekakuorth] [int] NULL,[movekakupara] [int] NULL,bitmask int null)
+	;
+DECLARE @rangelimits AS TABLE (mover bit,koma smallint, position smallint, moves smallint,direction int,distance int, distindex int)
+	;
+INSERT INTO @threatranges
+	SELECT kyokumen.*,moves._id moves_id,moves.moves,moves.direction,moves.distance,moves.movesuji,moves.movedan,moves.movekakuorth,moves.movekakupara,moves.bitmask
+	FROM @longrange kyokumen
+	JOIN moves ON moves.mover = kyokumen.mover and moves.position = kyokumen.position and moves.koma = kyokumen.koma
+	WHERE kyokumen.position BETWEEN 11 AND 99
+	;
+WITH cte AS (
+	SELECT threatranges.mover,threatranges.koma,threatranges.position,moves,direction,distance
+		, ROW_NUMBER() OVER (PARTITION BY threatranges.mover,threatranges.koma,threatranges.position,threatranges.direction ORDER BY distance) distindex
+	FROM @threatranges threatranges
+	JOIN @kyokumen kyokumen ON kyokumen.position=threatranges.moves
+	) INSERT INTO @rangelimits
+	SELECT * FROM cte WHERE distindex IN (1,2)
+	;
+DELETE moves1
+--select *
+from @rangelimits rangelimits1
+join @rangelimits rangelimits2 on rangelimits2.mover=rangelimits1.mover and rangelimits2.koma=rangelimits1.koma
+	and rangelimits2.position=rangelimits1.position and rangelimits2.direction=rangelimits1.direction
+	and rangelimits1.distindex=1 and rangelimits2.distindex=2
+join @kyokumen ou on ou.koma in (1,2) and ou.position=rangelimits2.moves and ou.mover=~rangelimits2.mover and rangelimits2.distindex = 2
+join @moves moves1 on moves1.position=rangelimits1.moves and moves1.mover=~rangelimits1.mover
+	and not (moves1.direction = rangelimits2.direction or (moves1.direction/4=rangelimits2.direction/4 and abs(moves1.direction-rangelimits2.direction)=2))
+	--Directions: N-E-S-W (0,1,2,3) and NE-SE-SW-NW (4,5,6,7)
+--	FROM @moves moves1
+--	JOIN @rangelimits rangelimits ON rangelimits.mover = ~moves1.mover --AND rangelimits.koma=moves1.koma
+--		AND rangelimits.moves = moves1.position AND rangelimits.distindex = 1
+	--JOIN @rangelimits ou ON rangelimits.mover = ~moves1.mover --AND rangelimits.koma=moves1.koma
+	--	AND rangelimits.moves = moves1.position AND rangelimits.distindex = 2 AND ou.koma IN (1,2)
+	--;
+DELETE moves1
+	FROM @moves moves1
 	--join threatranges on moves1.moves_id=moves1.moves_id
-	join rangelimits on rangelimits.mover = moves1.mover and rangelimits.koma=moves1.koma
-		and rangelimits.position = moves1.position and rangelimits.direction = moves1.direction
-	where moves1.distance > rangelimits.mindist;
+	JOIN @rangelimits rangelimits ON rangelimits.mover = moves1.mover AND rangelimits.koma=moves1.koma
+		AND rangelimits.position = moves1.position AND rangelimits.direction = moves1.direction AND rangelimits.distindex = 1
+	WHERE moves1.distance > rangelimits.distance
+	;
 --INSERT INTO @deleted_moves
 --	SELECT moves.*,CASE WHEN posn.mover = moves.mover THEN 'Self capture' ELSE 'Drop capture' END
 DELETE moves1
@@ -73,12 +97,8 @@ DELETE moves1
 	WHERE (	posn.mover = moves1.mover -- Remove self captures
 		OR	posn.next_mover= moves1.position ) -- Remove drop captures
 	;
-
---DELETE moves FROM @moves moves JOIN @deleted_moves deleted ON deleted.moves_id = moves.moves_id
---	;
--- Remove nifu
---INSERT INTO @deleted_moves
---	SELECT moves.*, 'Nifu'
+--DELETE moves FROM @moves moves JOIN @deleted_moves deleted ON deleted.moves_id = moves.moves_id;
+-- Remove nifu INSERT INTO @deleted_moves SELECT moves.*, 'Nifu';
 DELETE moves1
 	FROM @moves moves1
 	JOIN @player posn ON posn.koma = moves1.koma AND posn.suji = moves1.movesuji
